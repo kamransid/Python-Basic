3bba500e3c8bac7cfec55829f54a544efa90377c
md kamran siddique <ksid0001@ipv6-test.local>
2019-02-02 17:21:23 +0530

    First Finished

--- control_flow.py
+++ control_flow.py
@@ -65,5 +65,85 @@ def fib(n):
         a,b = b, a+b
     print()
 
-fib(2000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+fib(2000)
+print(fib(10))
+
+def fib2(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a,b = b, a+b
+    return result
+print(len(fib2(10)))
+s = 'Baital'
+print(s.__len__())
+#print(pair['alu'])
+
+s = [10, 'hola', {'pillu':'mantri'}, 90]
+print(s.__len__())
+
+touple = (1,2,{'hola':'hi'}, 'kulli')
+print(type(touple))
+print(touple[2])
+print(touple[-1])
+print(touple[:])
+print(touple.__len__())
+
+li = [1,list(touple)]
+print(li)
+
+dic = {1:2,3:4}
+print(type(dic))
+
+dic = {1,2}
+print(type(dic))
+print('*****Type Empty******')
+dic={1:'paltu'}
+print(type(dic))
+dic = []
+print(type(dic))
+dic = ()
+print(type(dic))
+
+
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries -1
+        if retries < 0 :
+            raise ValueError()
+
+# ask_ok('Do you really want to quit?')
+
+
+i = 5
+
+def f(val=i):
+    print(val)
+i='vante matram'
+f()
+
+print('*****Another Session******')
+print('*****Another Session******')
+
+def f_list(a, L=[]):
+    L.append(a)
+    return L
+
+print(f_list(1))
+print(f_list(2))
+print(f_list(3))
+s = None
+print(type(s))
+print(s)
+
+
+
+
+
 
