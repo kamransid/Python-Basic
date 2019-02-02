6ffdefc93bc2288c57ec65f8220ff96ca09662ca
md kamran siddique <ksid0001@ipv6-test.local>
2019-02-02 11:13:40 +0530

    First Commit

---
+++ intro.py
@@ -0,0 +1,63 @@
+# # x = 2+2
+# # print(x)
+
+# # y = 50 - 5*6
+
+# # print(y)
+
+# # y = (50-5*6)/4
+# # print(y)
+
+# final = 8/5
+# print(final)
+# final = 8//5
+# print(final)
+# key = 17/3
+# print(key)
+# key = 17%3
+# print(key)
+# power = 2559999999999999999876876755645**2
+# print(power)
+
+# # tax = 'string'
+# # print(tax.count())
+
+# tax = 12.5/100
+# print(tax)
+# price = 100.50
+# print(price*tax)
+
+# #print(price+_)#Error
+
+# stri = '"Isn\'t," they said'
+# print(stri)
+
+# l = 'first line \n secon line'
+#     print(l)
+print("""
+
+  Kuch bhi and      bhi
+  hoga 
+  """
+ )
+
+s = 'kamran'*3
+print(s)
+
+k = 3 * 'un' + 'ium'
+print(k)
+p = 'un'*(8//4)
+mai = 'hana'
+kar = 'hua' 'hai' +mai+ 'kuch bhi hota hai' 
+print(kar)
+
+
+
+
+
+
+
+
+
+
+
